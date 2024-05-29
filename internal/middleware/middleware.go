84d00a51d929970faa52ee3d841db8f224229f33
Maxim <lolerpro4@gmail.com>
2024-05-29 23:13:46 +0300

    [feet]: Added middleware for logging

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+// LoggingMiddleware Middleware for logging requests
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rw := newResponseWriter(w)
+		next.ServeHTTP(rw, r)
+
+		log.Printf("Completed `%s` method=%s code=%d time=%s", r.RequestURI, r.Method, rw.statusCode, time.Since(start))
+	})
+}
