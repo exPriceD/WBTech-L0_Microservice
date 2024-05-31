ebae21ce3596fce26539376208cc014bf76a9406
Maxim <lolerpro4@gmail.com>
2024-06-01 01:48:39 +0300

    [fix]: Fixed schema.sql

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -1,49 +1,7 @@
-CREATE TABLE delivery
-(
-    delivery_id INTEGER PRIMARY KEY,
-    name      VARCHAR,
-    phone     VARCHAR,
-    zip       VARCHAR,
-    city      VARCHAR,
-    address   VARCHAR,
-    region    VARCHAR,
-    email     VARCHAR
-);
-
-CREATE TABLE payment
-(
-    transaction   VARCHAR PRIMARY KEY,
-    request_id    VARCHAR,
-    currency      VARCHAR,
-    provider      VARCHAR,
-    amount        INTEGER,
-    payment_dt    INTEGER,
-    bank          VARCHAR,
-    delivery_cost INTEGER,
-    goods_total   INTEGER,
-    custom_fee    INTEGER
-);
-
-CREATE TABLE items
-(
-    chrt_id      INTEGER PRIMARY KEY,
-    track_number VARCHAR UNIQUE,
-    price        INTEGER,
-    rid          VARCHAR,
-    name         VARCHAR,
-    sale         INTEGER,
-    size         VARCHAR,
-    total_price  INTEGER,
-    nm_id        INTEGER,
-    brand        VARCHAR,
-    status       INTEGER
-);
-
 CREATE TABLE orders
 (
     order_uid          VARCHAR PRIMARY KEY,
     track_number       VARCHAR,
-    delivery_id        INTEGER,
     entry              VARCHAR,
     locale             VARCHAR,
     internal_signature VARCHAR,
@@ -52,8 +10,51 @@ CREATE TABLE orders
     shardkey           VARCHAR,
     sm_id              INTEGER,
     date_created       TIMESTAMP,
-    oof_shard          VARCHAR,
-    FOREIGN KEY (delivery_id) REFERENCES delivery (delivery_id),
-    FOREIGN KEY (order_uid) REFERENCES payment (transaction),
-    FOREIGN KEY (track_number) REFERENCES items (track_number)
+    oof_shard          VARCHAR
+);
+
+CREATE TABLE delivery
+(
+    delivery_id SERIAL UNIQUE PRIMARY KEY,
+    order_uid   VARCHAR,
+    name        VARCHAR,
+    phone       VARCHAR,
+    zip         VARCHAR,
+    city        VARCHAR,
+    address     VARCHAR,
+    region      VARCHAR,
+    email       VARCHAR,
+    FOREIGN KEY (order_uid) REFERENCES orders (order_uid)
+);
+
+CREATE TABLE payment
+(
+    transaction   VARCHAR UNIQUE PRIMARY KEY,
+    request_id    VARCHAR,
+    currency      VARCHAR,
+    provider      VARCHAR,
+    amount        INTEGER,
+    payment_dt    INTEGER,
+    bank          VARCHAR,
+    delivery_cost INTEGER,
+    goods_total   INTEGER,
+    custom_fee    INTEGER,
+    FOREIGN KEY (transaction) REFERENCES orders (order_uid)
+);
+
+CREATE TABLE items
+(
+    chrt_id      INTEGER PRIMARY KEY,
+    order_uid    VARCHAR,
+    track_number VARCHAR,
+    price        INTEGER,
+    rid          VARCHAR,
+    name         VARCHAR,
+    sale         INTEGER,
+    size         VARCHAR,
+    total_price  INTEGER,
+    nm_id        INTEGER,
+    brand        VARCHAR,
+    status       INTEGER,
+    FOREIGN KEY (order_uid) REFERENCES orders (order_uid)
 );
