b1466f407783edde398605015ca0579a3d6006a8
Maxim <lolerpro4@gmail.com>
2024-06-09 11:39:58 +0300

    [feet]: Added a script for publishing order data to the channel

---
+++ natsStreaming/nats.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/nats-io/stan.go"
+)
+
+func main() {
+	filePath := "order.json"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	clusterID := "test-cluster"
+	clientID := "publisher-client"
+	sc, err := stan.Connect(clusterID, clientID)
+	if err != nil {
+		log.Fatalf("Failed to connect to NATS Streaming: %v", err)
+	}
+	defer func(sc stan.Conn) {
+		_ = sc.Close()
+	}(sc)
+
+	channel := "orders"
+	err = sc.Publish(channel, data)
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+	}
+
+	log.Printf("Message published to channel %s: %s", channel, string(data))
+}
